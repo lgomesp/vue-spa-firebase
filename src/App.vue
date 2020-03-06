748972175189663480e62054cc81575c31c0f77d
Lucas Gomes <lucasgomesp22@gmail.com>
2020-03-06 17:44:19 -0300

    add navbar

--- src/App.vue
+++ src/App.vue
@@ -1,9 +1,21 @@
 <template>
   <div id="app">
+    <Navigation />
     <router-view class="container" />
   </div>
 </template>
 
+<script>
+import Navigation from "@/components/Navigation";
+
+export default {
+  name: "app",
+  components: {
+    Navigation
+  }
+};
+</script>
+
 <style lang="scss">
-@import "node_modules/bootstrap/scss/bootstrap"
+@import "node_modules/bootstrap/scss/bootstrap";
 </style>
--- src/components/Navigation.vue
+++ src/components/Navigation.vue
@@ -1,3 +1,14 @@
 <template>
-<h1>navigation</h1>    
+  <div>
+    <nav class="site-nav navbar navbar-expand bg-primary navbar-dark">
+      <div class="container-fluid">
+        <router-link class="navbar-brand" to="/">Meeting Log</router-link>
+        <div class="navbar-nav ml-auto">
+          <router-link class="nav-item nav-link" to="/meetings">meetings</router-link>
+          <router-link class="nav-item nav-link" to="/login">login</router-link>
+          <router-link class="nav-item nav-link" to="/register">register</router-link>
+        </div>
+      </div>
+    </nav>
+  </div>
 </template>
